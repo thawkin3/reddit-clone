00190c25ac1b77ad6371d7118a4f5c410c990df4
Tyler Hawkins <thawkin3@byu.net>
2022-02-15 20:55:00 -0700

    refactor: removes usage of Card component by Subreadit component

--- src/components/Subreadit.js
+++ src/components/Subreadit.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Card } from './Card'
 import './Subreadit.css'
 
 export const Subreadit = ({ isPreview = false, title, description }) => (
-  <Card>
+  <div className="subreaditCard">
     {isPreview ? (
       <h3 className="subreaditTitle">
         <Link to={`/subreadit/${title}`}>r/{title}</Link>
@@ -13,5 +12,5 @@ export const Subreadit = ({ isPreview = false, title, description }) => (
       <h1 className="subreaditTitle">r/{title}</h1>
     )}
     <p className="subreaditDescription">{description}</p>
-  </Card>
+  </div>
 )
